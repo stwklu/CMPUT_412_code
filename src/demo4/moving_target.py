12f558d508b0be03503d4e90d1e13e4da186bef4
kamenriderdrive <438784575@qq.com>
2019-02-28 10:30:04 -0700

    Add files via upload

--- src/demo4/moving_target.py
+++ src/demo4/moving_target.py
@@ -2,13 +2,18 @@
 import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from sensor_msgs.msg import Joy
 
 waypoints = [
-    [(6.55080, -3.78138, 0.0), (0.0, 0.0, -0.15988, 0.98714)],
-    [(5.17323, -5.34717, 0.0), (0.0, 0.0, -0.78542, 0.61896)],
-    [(3.37294, -4.97941, 0.0), (0.0, 0.0, -0.83292, 0.55339)],
-    [(4.32570, -3.82070, 0.0), (0.0, 0.0, -5.11518, 0.85927)]
+    [(3.24835373927, -1.40084858646, 0.0), (0.0, 0.0, -0.804220067176, 0.594331627588)],  # top left
+    [(1.74468228844, -2.84314082869, 0.0), (0.0, 0.0, 0.989949746297, 0.141419587774)],  # top right
+    [(0.945313977309, -1.26111663468, 0.0), (0.0, 0.0, 0.854504163474, 0.519444544302)],  # bottom left
+    [(-0.0848826540415, -2.39410610964, 0.0), (0.0, 0.0, -0.798635510047, 0.601815023152)]  # bottom right
 ]
+
+order_list = []
+
+is_moving = False
 
 
 def goal_pose(pose):
@@ -24,12 +29,33 @@ def goal_pose(pose):
     return goal_pose
 
 
+def joy_callback(msg):
+    global order_list, is_moving
+
+    if (msg.buttons[0] == 1) and (0 not in order_list):
+        order_list.append(0)
+    elif (msg.buttons[1] == 1) and (1 not in order_list):
+        order_list.append(1)
+    elif (msg.buttons[2] == 1) and (2 not in order_list):
+        order_list.append(2)
+    elif (msg.buttons[3] == 1) and (3 not in order_list):
+        order_list.append(3)
+    else:
+        pass
+
+    if msg.axes[5] == 1.0:
+        is_moving = (not is_moving)
+
+
 if __name__ == '__main__':
-    rospy.init_node('patrol')
+    rospy.init_node('naving_square')
+    rospy.Subscriber("/joy", Joy, joy_callback)
     client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
     client.wait_for_server()
-    while True:
-        for pose in waypoints:
-            goal = goal_pose(pose)
-            client.send_goal(goal)
-            client.wait_for_result()
+    while not rospy.is_shutdown():
+
+        if is_moving and len(order_list) == 4:
+            for index in order_list:
+                goal = goal_pose(waypoints[index])
+                client.send_goal(goal)
+                client.wait_for_result()
